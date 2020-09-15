9857ea280b08afd2522ea9f59f41776b253faeb8
Lais Dias <laisdias099@gmail.com>
2020-09-14 21:15:39 -0300

    correção no código

--- aula07/aula07-exercicio1/src/Exercicio1.kt
+++ aula07/aula07-exercicio1/src/Exercicio1.kt
@@ -3,12 +3,10 @@ fun main(){
     var cliente2 = Cliente ("Maria", "Souza")
 
     var conta1 = Conta (cliente1, 123, 5000.0)
+    conta1.deposito(500.0)
+    conta1.saque(50.0)
 
     var conta2 = Conta (cliente2, 321, 1000.0)
-
-    conta1.deposito(500.0)
     conta2.deposito(20.0)
-
-    conta1.saque(50.0)
     conta2.saque(500.0)
 }
